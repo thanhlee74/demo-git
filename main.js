d7e10136854c0eec3cfaf940505e4eb9879586c6
thanhlee74 <thanhhx1998@gmail.com>
2021-05-25 09:20:19 +0700

    thay doi mot so thu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("chao` mung` ban. den' voi' chung' toi")
+
+
+var rs=NaN || 'b' && 'c';
+var a= 'thanh';
+if (a==rs) {
+    console.log("thanh dep trai");
+}
+console.log(a);
+console.log(rs);
